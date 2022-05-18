cb45079c1f9a94377b97ac93a400cb983515e868
Shivam <shivam.sinha@zemosolabs.com>
2022-05-18 15:24:50 +0530

    pushing my project files in github

---
+++ src/main/java/com/smart/config/MySecurityConfig.java
@@ -0,0 +1,55 @@
+package com.smart.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import com.smart.service.UserDetailsServiceImpl;
+
+@Configuration
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public UserDetailsService getUserDetailService() {
+		return new UserDetailsServiceImpl();
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+		daoAuthenticationProvider.setUserDetailsService(this.getUserDetailService());
+		daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+		return daoAuthenticationProvider;
+
+	}
+
+	/// configuring method...
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(authenticationProvider());
+
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/user/**").hasRole("USER")
+				.antMatchers("/**").permitAll().and().formLogin()
+				.loginPage("/signin") //custom login page
+				.loginProcessingUrl("/dologin")//the url to submit the username and pass to
+				.defaultSuccessUrl("/user/index")	// my landing page
+				.and().csrf().disable();
+	}
+
+}
